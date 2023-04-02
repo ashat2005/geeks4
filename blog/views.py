1bf866ca86897bf37f734673d68a9d66f34d151c
ashatkydyrov61@gmail.com <ashatkydyrov61@gmail.com>
2023-04-02 01:08:25 -0500

    dz2

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+# from django.shortcuts import render
+from django.http import HttpResponse
 # Create your views here.
+def get_hello(request):
+    return  HttpResponse("Hello", headers={"name": "alex"}, status=500)
+
+def get_contacts(request):
+    return HttpResponse("number")
+
+def get_about(request):
+    return HttpResponse("about")
